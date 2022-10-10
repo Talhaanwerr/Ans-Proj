6f0513fb57d87eefac48343087799f7ce79b04f2
Talha Anwer <talha.anwar@symits@gmail.com>
2022-10-10 05:07:23 +0500

    Post work in progress

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -26,9 +26,9 @@ exports.getAll = async (req, res) => {
     try {
         let posts = await Post
             .findAll({
-                  include: [
-                    [{ model: User }]
-                  ]
+                //   include: [
+                //     [{ model: User }]
+                //   ]
             })
             // .populate("client personnel", '-password');
 
@@ -38,3 +38,67 @@ exports.getAll = async (req, res) => {
         return res.status(400).json({ errors: error.message });
     }
 }
+
+exports.getById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        let post = await Post
+            .findOne({ where: { id: id } })
+            // .populate("client personnel", '-password');
+        if (post == null) {
+            return res.status(404).json({ errors: [{ message: "Post does not exist..." }] });
+        }
+        return res.status(200).json({ post });
+    } catch (error) {
+        return res.status(400).json({ errors: error.message });
+    }
+}
+
+exports.deleteById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        let post = await Post.findOne({ where: { id }});
+        if (post == null) {
+            return res.status(404).json({ errors: [{ message: "Post does not exist..." }] });
+        }
+
+        if(req.user.id != post.userId){
+            return res.status(403).send({ status: "error", msg: "You are not authorized to delete this post" });
+        }
+
+        let deletedPost = await Post.destroy({ where: { id }});
+        if(deletedPost){
+            return res.status(200).json({ success: "Post Deleted Successfully", deletedPost });
+        }
+
+    } catch (error) {
+        return res.status(400).json({ errors: error.message });
+    }
+}
+
+exports.updateById = async (req, res) => {
+    const { id } = req.params;
+    const query = req.body
+    
+    try {
+        let post = await Post.findOne({ where: { id }});
+        if (post == null) {
+            return res.status(404).json({ errors: [{ message: "Post does not exist..." }] });
+        }
+
+        if(req.user.id != post.userId){
+            return res.status(403).send({ status: "error", msg: "You are not authorized to update this post" });
+        }
+
+        let updatedPost = await await Post.update(query, { where: { id }});
+        if(updatedPost){
+            return res.status(200).json({ success: "Post Updated Successfully", updatedPost });
+        }
+
+
+    } catch (error) {
+        return res.status(400).json({ errors: error.message });
+    }
+}
+
--- router/post.route.js
+++ router/post.route.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { create, getAll } = require('../controllers/post.controller')
+const { create, getAll, getById, deleteById, updateById } = require('../controllers/post.controller')
 const authenticateToken = require('../middleware/authenticate.middleware')
 
 
 router.post('/', authenticateToken, create);
 router.get('/', authenticateToken, getAll);
+router.get('/:id', authenticateToken, getById);
+router.delete('/:id', authenticateToken, deleteById);
+router.patch('/:id', authenticateToken, updateById);
+
 
 
 module.exports = router
